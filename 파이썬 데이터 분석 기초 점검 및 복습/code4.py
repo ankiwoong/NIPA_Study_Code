264178b8f6bdfdf381c7509407cdf40c325e5e37
Ankiwoong <ankiwoong@gmail.com>
2020-09-14 08:35:56 +0900

    리스트의 활용

---
+++ 파이썬 데이터 분석 기초 점검 및 복습/code4.py
@@ -0,0 +1,12 @@
+# 여기 빈 리스트가 있습니다.
+number_list = []
+
+# number_list에 1부터 100까지 수를 거꾸로 넣어보세요
+# 예: 100, 99, 98, ... 3, 2, 1
+for i in range(0, 100):
+    number_list.append(100 - i)
+
+# number_list에서 10번째, 30번째, 50번째 원소를 각각 출력해봅시다
+print(number_list[9])  # 10번째 원소
+print(number_list[29])  # 30번째 원소
+print(number_list[49])  # 50번째 원소
