952781dad5a8b35f67a1d0f6ddfed2744002254e
Ankiwoong <ankiwoong@gmail.com>
2020-09-14 08:35:22 +0900

    변수와 연산자

---
+++ 파이썬 데이터 분석 기초 점검 및 복습/code2.py
@@ -0,0 +1,9 @@
+# num1 변수를 선언하고, num1 변수에 정수 10을 대입합니다.
+num1 = 10
+# num2 변수를 선언하고, num2 변수에 정수 5을 대입합니다.
+num2 = 5
+# num1과 num2를 더하고, 더한 값에 10을 곱한 값을 result 변수에 저장합니다.
+result = (num1 + num2) * 10
+
+# result 값을 출력합니다.
+print(result)
