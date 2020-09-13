0ebbcbd39a430f0fd7d02a6f38397dd95d122a68
Ankiwoong <ankiwoong@gmail.com>
2020-09-14 08:36:19 +0900

    나만의 함수 만들기

---
+++ 파이썬 데이터 분석 기초 점검 및 복습/code5.py
@@ -0,0 +1,13 @@
+# 입력 숫자
+input_num = 10
+
+# 숫자 데이터를 넣었을 때 10배로 되돌려주는 함수
+def my_function(n):
+    return n * 10
+
+
+# my_function을 사용하여 result에 값을 저장합니다.
+result = my_function(input_num)
+
+# 최종 결과값을 출력합니다.
+print(result)
