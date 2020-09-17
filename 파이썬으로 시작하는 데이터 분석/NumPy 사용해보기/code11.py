83926b85cbfabfaf1781b6e2c5ad98e2869f83fa
Ankiwoong <ankiwoong@gmail.com>
2020-09-17 18:06:39 +0900

    [실습7] Numpy 연산

---
+++ 파이썬으로 시작하는 데이터 분석/NumPy 사용해보기/code11.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+print(array)
+
+# Q1. array에 5를 더한 값을 출력해보세요.
+print(array + 5)
+
+# Q2. array에 5를 뺀 값을 출력해보세요.
+print(array - 5)
+
+# Q3. array에 5를 곱한 값을 출력해보세요.
+print(array * 5)
+
+# Q4. array를 5로 나눈 값을 출력해보세요.
+print(array / 5)
+
+# Q5. array에 array2를 더한 값을 출력해보세요.
+array2 = np.array([5, 4, 3, 2, 1])
+print(array + array2)
+
+# Q6. array에 array2를 뺀 값을 출력해보세요.
+print(array - array2)
