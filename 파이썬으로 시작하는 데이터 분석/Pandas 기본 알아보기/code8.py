aedfb4041c1fa874748be0291b2f02e727a49a16
ankiwoong <ankiwoong@gmail.com>
2020-09-24 08:27:56 +0900

    [실습4] pandas 연산과 함수

---
+++ 파이썬으로 시작하는 데이터 분석/Pandas 기본 알아보기/code8.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+print("A: ")
+A = pd.DataFrame(np.random.randint(0, 10, (2, 2)), columns=["A", "B"])  # 칼럼이 A, B입니다.
+print(A, "\n")
+
+
+print("B: ")
+B = pd.DataFrame(
+    np.random.randint(0, 10, (3, 3)), columns=["B", "A", "C"]
+)  # 칼럼이 B, A, C입니다.
+print(B, "\n")
+
+
+# 아래에 다양한 연산을 자유롭게 적용해보세요.
+print(A + B, "\n")
+print(A.add(B, fill_value=0), "\n")
