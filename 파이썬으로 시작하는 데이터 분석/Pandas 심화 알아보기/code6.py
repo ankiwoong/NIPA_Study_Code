5e4cbdde942c38aba5e75139adb2d641dc89f6f6
AnKiWoong <ankiwoong@gmail.com>
2020-10-05 08:51:36 +0900

    [실습3] 그룹으로 묶기(1)

---
+++ 파이썬으로 시작하는 데이터 분석/Pandas 심화 알아보기/code6.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "key": ["A", "B", "C", "A", "B", "C"],
+        "data1": [1, 2, 3, 1, 2, 3],
+        "data2": [4, 4, 6, 0, 6, 1],
+    }
+)
+print("DataFrame:")
+print(df, "\n")
+
+# groupby 함수를 이용해봅시다.
+# key를 기준으로 묶어 합계를 구해 출력해보세요.
+print(df.groupby("key").sum(), "\n")
+
+
+# key와 data1을 기준으로 묶어 합계를 구해 출력해보세요.
+print(df.groupby(["key", "data1"]).sum(), "\n")
