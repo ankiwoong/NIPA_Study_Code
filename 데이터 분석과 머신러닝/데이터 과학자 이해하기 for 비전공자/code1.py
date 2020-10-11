cca2a0e9567104b36ab9bc23c492bc3a714bafb8
AnKiWoong <ankiwoong@gmail.com>
2020-10-11 16:50:59 +0900

    [실습1-3] 선택한 3가지 요인의 실제 연관성 평가하기

---
+++ 데이터 분석과 머신러닝/데이터 과학자 이해하기 for 비전공자/code1.py
@@ -0,0 +1,12 @@
+from heart_disease import *
+
+
+def result_of_heart_disease_prediction():
+    # 선택한 3개의 요인이 실제 심장질환에 얼마나 관련성이 있는지 확인합니다.
+    # Example: features = ['age', 'sex', 'blood_pressure']
+    features = ["thal", "chest_pain", "vessels"]
+    check_3_features(features)
+
+
+if __name__ == "__main__":
+    result_of_heart_disease_prediction()
